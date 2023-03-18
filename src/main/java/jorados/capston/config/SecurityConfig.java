7ea6522f67675cd4c01fec1e8dcff4b970cba948
seongjin8860 <seongjin8860@naver.com>
2023-03-19 00:42:38 +0900

    login컨트롤러 구현

--- src/main/java/jorados/capston/config/SecurityConfig.java
+++ src/main/java/jorados/capston/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -42,6 +43,11 @@ public class SecurityConfig {
             http.addFilter(new JwtAuthenticationFilter(authenticationManager));
             http.addFilter(new JwtAuthorizationFilter(authenticationManager,userRepository));
         }
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
--- src/main/java/jorados/capston/controller/UserController.java
+++ src/main/java/jorados/capston/controller/UserController.java
@@ -16,11 +16,16 @@ import jorados.capston.response.UserResponse;
 import jorados.capston.service.UserService;
 import jorados.capston.util.CustomResponseUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.IOException;
@@ -28,6 +33,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
+@Slf4j
 //@RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
@@ -35,6 +41,8 @@ public class UserController {
     private final UserService userService;
     private final UserRepository userRepository;
     private final PrincipalDetailsService principalDetailsService;
+
+    private final AuthenticationManager authenticationManager;
 
     //login,logout 기능은 일단 스프링시큐리티에서 제공
 
@@ -50,16 +58,20 @@ public class UserController {
         return new ResponseEntity<>(new ResponseDto<>(1, "회원가입 성공", user.getUsername()), HttpStatus.CREATED);
     }
 
-    @PostMapping("/login2") //->비밀번호 이슈
+    @PostMapping("/login2") //->비밀번호 이슈 //비밀번호 안치거나 안맞아도 username만 맞으면 login 진행됨.
     public void login(@RequestBody @Valid User user,HttpServletResponse response){
-        System.out.println("/login 컨트롤러 진입 테스트");
+        System.out.println("/login 컨트롤러 진입");
         String username = user.getUsername();
         String password = user.getPassword();
 
+        //1.userId로 정보가져오기
         UserDetails loginUser = principalDetailsService.loadUserByUsername(username);
+        //2.가져온 정보와 입력한 비밀번호로 검증 --> username,password 일치하면 토큰발급
+
         User findUser = userRepository.findByUsername(username).orElseThrow(() -> new UserNotFound());
         PrincipalDetails principalDetails = new PrincipalDetails(findUser);
 
+        //4.토큰 생성후 발급
         String jwtToken = JWT.create()
                 .withSubject(loginUser.getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis()+ JwtProperties.EXPIRATION_TIME))
@@ -69,6 +81,31 @@ public class UserController {
 
         response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX+jwtToken);
         CustomResponseUtil.success(response, loginUser);
+    }
+
+
+    //문제점 : id,pw일치하지 않으면 401에러 발생시켜야함.
+    @PostMapping("/login3")
+    public void login3(HttpServletRequest request,HttpServletResponse response) throws IOException {
+        ObjectMapper om = new ObjectMapper();
+        User user = om.readValue(request.getInputStream(), User.class);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword());
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        log.info("로그인 완료 = {}",principalDetails.getUser().getUsername());
+
+        //4.토큰 생성후 발급
+        String jwtToken = JWT.create()
+                .withSubject(principalDetails.getUser().getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()+ JwtProperties.EXPIRATION_TIME))
+                .withClaim("id", principalDetails.getUser().getId())
+                .withClaim("username", principalDetails.getUser().getUsername())
+                .sign(Algorithm.HMAC512(JwtProperties.SECRET));
+
+        response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX+jwtToken);
+        CustomResponseUtil.success(response, principalDetails.getUser());
     }
 
     //권한 테스트
@@ -111,7 +148,7 @@ public class UserController {
     public void deleteUser(@PathVariable Long userId){
         userService.delete(userId);
     }
-//
+
 //    //로그인 성공 -> 유저 정보 일부 반환
 //    @GetMapping("/user/success/{userId}")
 //    public UserResponse success(){
