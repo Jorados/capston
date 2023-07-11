9fdf029845ba7ca1538bccbb23434931d3e6dddb
seongjin8860 <seongjin8860@naver.com>
2023-07-11 23:32:19 +0900

    배포 자동화 제발 git add .git add . 5

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ PROJECT_NAME=capston
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/ZIP/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 어플리케이션 pid 확인"
 
@@ -23,8 +23,7 @@ fi
 
 echo "> 새 어플리케이션 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
-
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
